79737ae73d6af8af5471897f42cb8c74e4cc999f
AndreToral <jrodriguez16@upao.edu.pe>
2025-09-14 12:42:44 -0500

    docs(database): añadir comentarios explicativos en database.tf

--- database.tf
+++ database.tf
@@ -1,19 +1,26 @@
+# Contenedor de Redis
 resource "docker_container" "redis" {
   name  = "redis"
   image = var.redis_image
+
+  # Conexion a la red persistencia (persistence_net)
   networks_advanced {
     name = docker_network.persistence_net.name
   }
 }
 
+# Contenedor de PostgreSQL
 resource "docker_container" "postgres" {
   name  = "postgres"
   image = var.postgres_image
+  
+  # Variables de entorno para inicializar usuario, password y DB
   env = [
     "POSTGRES_USER=${var.postgres_user}",
     "POSTGRES_PASSWORD=${var.postgres_password}",
     "POSTGRES_DB=${var.postgres_db}"
   ]
+
   networks_advanced {
     name = docker_network.persistence_net.name
   }
