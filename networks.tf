5be6ef7e9522d7c4efcb382cb200df2d6d6d2194
AndreToral <jrodriguez16@upao.edu.pe>
2025-09-14 12:47:07 -0500

    docs(networks): añadir comentarios explicativos sobre definicion de redes de aislamiento

--- networks.tf
+++ networks.tf
@@ -1,11 +1,18 @@
+##########################################
+#   Definicion de redes de aislamiento   #
+##########################################
+
+# Red para las aplicaciones (app1, app2, app3)
 resource "docker_network" "app_net" {
   name = var.app_net_name
 }
 
+# Red para las bases de datos (Redis y PostgreSQL)
 resource "docker_network" "persistence_net" {
   name = var.persistence_net_name
 }
 
+# Red para el monitoreo (Grafana)
 resource "docker_network" "monitor_net" {
   name = var.monitor_net_name
 }
