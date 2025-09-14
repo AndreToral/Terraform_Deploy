e76575486ab86244fa901a4d468836db86b09b15
AndreToral <jrodriguez16@upao.edu.pe>
2025-09-14 12:43:32 -0500

    docs(monitoring): añadir comentarios explicativos en monitoring.tf

--- monitoring.tf
+++ monitoring.tf
@@ -1,9 +1,13 @@
+# Contenedor de Grafana expuesto en el puerto 3000
 resource "docker_container" "grafana" {
   name  = "grafana"
   image = var.grafana_image
+
+  # Conexion a la red de monitoreo (monitor_net)
   networks_advanced {
     name = docker_network.monitor_net.name
   }
+
   ports {
     internal = 3000
     external = 3000
