64785ce4853cc91036bee760241b8eaba9a15fa0
AndreToral <jrodriguez16@upao.edu.pe>
2025-09-13 20:02:50 -0500

    feat(variables): añadir nuevas variables redis_image, postgres_image y sus variables de conexion

--- variables.tf
+++ variables.tf
@@ -5,6 +5,39 @@ variable "app_image" {
   default = "nginx:stable-perl"
 }
 
+# Variables de Databases
+variable "redis_image" {
+  description = "Imagen de Redis"
+  type        = string
+  default = "redis:8.2.1"
+}
+
+variable "postgres_image" {
+  description = "Imagen de PostgreSQL"
+  type        = string
+  default = "postgres:13-trixie"
+}
+
+# Variables de conexion a postgreSQL
+variable "postgres_user" {
+  description = "Usuario de PostgreSQL"
+  type        = string
+  default = "devuser"
+}
+
+variable "postgres_password" {
+  description = "Contraseña de PostgreSQL"
+  type        = string
+  sensitive   = true
+  default = "devpass"
+}
+
+variable "postgres_db" {
+  description = "Base de datos por defecto"
+  type        = string
+  default = "devdb"
+}
+
 # Variables de redes
 variable "app_net_name" {
   description = "Nombre de la red para las apps"
